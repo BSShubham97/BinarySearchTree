f73ebde0ed8c6d3293071a643c3d494aab7bc82d
Shubham Bandagale <75619081+BSShubham97@users.noreply.github.com>
2021-09-03 00:44:55 +0530

    Added: insert() into BST

---
+++ BSTmain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class BSTmain {
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+
+        binarySearchTree.add(56);
+        binarySearchTree.add(30);
+        binarySearchTree.add(70);
+
+    }
+}
